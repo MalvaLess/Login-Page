2f2bd9b0297c6bc8faa04ed03ca5f0b4f0176ac0
MalvaLess <spider3899@gmail.com>
2026-01-22 17:32:23 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+function handleLogin(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (username && password) {
+    alert(`Welcome, ${username}!`);
+    // Redirect or validate
+  } else {
+    alert("Please enter both username and password.");
+  }
+}
+
+// Password toggle
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("click", () => {
+  const isPassword = passwordInput.type === "password"; // ahora sí existe
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+
+  togglePassword.classList.toggle("fa-eye-slash");
+  togglePassword.classList.toggle("fa-eye");
+
+  // Dinamic aria-label
+  togglePassword.setAttribute("aria-label", isPassword ? "Show password" : "Hide password");
+
+});
+
